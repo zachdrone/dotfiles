a7953829c22627d5e55a522c2bbe3c8f1b4a1764
Zach Drone <drone@torc.ai>
2025-01-05 19:18:26 -0500

    add wezterm window option

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -11,7 +11,7 @@ config.font_size = 19
 
 config.enable_tab_bar = false
 
-config.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE" -- "TITLE | RESIZE"
 config.window_background_opacity = 1.0
 config.macos_window_background_blur = 10
 
